8b56e6d38e7003520f98a49386588984269c5f27
jooho2075 <parkjh2075@icloud.com>
2024-08-28 11:01:59 +0900

    24.08.28_if_statement

--- Running_Js_Script/app.js
+++ Running_Js_Script/app.js
@@ -1,4 +1,19 @@
 //168_Javascript실행하기
+/*
 console.log("Hello from our first file");
 let total = 1 + 3;
 console.log("Goodbye");
+*/
+
+//169_if구문
+//console.log("It's working");
+let random = Math.random();
+if(random < 0.5) {
+    console.log("your number is less than 0.5");
+    console.log(random);
+}
+
+if(random >= 0.5) {
+    console.log("your number is greater than 0.5");
+    console.log(random);
+}
