c807abce5af5c3afc9422641ca8a968eca3e309b
jooho2075 <parkjh2075@icloud.com>
2024-08-28 11:20:45 +0900

    24.08.28_elseif구문

--- Running_Js_Script/app.js
+++ Running_Js_Script/app.js
@@ -7,6 +7,7 @@ console.log("Goodbye");
 
 //169_if구문
 //console.log("It's working");
+/*
 let random = Math.random();
 if(random < 0.5) {
     console.log("your number is less than 0.5");
@@ -17,3 +18,27 @@ if(random >= 0.5) {
     console.log("your number is greater than 0.5");
     console.log(random);
 }
+*/
+
+//170_elseif구문
+/* const dayOfWeek = 'Friday';
+if(dayOfWeek === 'Monday') {
+    console.log("I hate Mondays")
+} else if(dayOfWeek === 'Saturday') {
+    console.log("I love Saturdays")
+} else if(dayOfWeek === 'Friday') {
+    console.log("I love Fridays")
+} 
+*/
+
+/* const age = 8;
+if(age < 5) {
+    console.log("Free");
+} else if(age < 10) {
+    console.log("Child");
+} else if(age < 65) {
+    console.log("Adult");
+} else if(age > 65) {
+    console.log("Senior");
+}
+*/
