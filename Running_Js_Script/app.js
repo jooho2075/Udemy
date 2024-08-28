dcd7d011ff7b72351d083ff23100e6c2ab9b05f3
jooho2075 <parkjh2075@icloud.com>
2024-08-28 12:31:38 +0900

    24.08.28_logicalOperators_Or

--- Running_Js_Script/app.js
+++ Running_Js_Script/app.js
@@ -116,9 +116,23 @@ if(null) {
 } */
 
 //174_logicalOperators_And
+/* 
 const password = prompt("Enter your password");
 if(password.length >= 6 && password.indexOf(' ') === -1) {
     console.log("valid password")
 } else {
     console.log("invalid password")
+}
+*/
+
+//175_logicalOperators_Or
+const age = -90;
+if(age >= 0 && age < 5 || age >= 65) {
+    console.log("free");
+} else if(age >= 5 && age < 10) {
+    console.log("$10");
+} else if(age >= 10 && age < 65) {
+    console.log("$20")
+} else {
+    console.log("invalid age")
 }
