a613d4a0d18051604a919ec5202c04e52943412d
jooho2075 <parkjh2075@icloud.com>
2024-08-30 11:41:32 +0900

    24.08.30_ArrayLoop

--- Loop/app.js
+++ Loop/app.js
@@ -30,3 +30,13 @@ for(let i = 100; i >= 0; i--) {
     console.log(i);
 } 
 */
+
+//198_ArrayLoop
+/* 
+const animal = ['lion', 'tiger', 'bear'];
+
+for(let i = 0; i < animal.length; i++) {
+    console.log(i + " : " + animal[i]);
+} 
+*/
+
