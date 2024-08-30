547cff5e6da12b783085f9c573ba88f27afd55fb
jooho2075 <parkjh2075@icloud.com>
2024-08-30 11:57:39 +0900

    24.08.30_NestedLoop

--- Loop/app.js
+++ Loop/app.js
@@ -40,3 +40,29 @@ for(let i = 0; i < animal.length; i++) {
 } 
 */
 
+//199_NestedLoop
+/* 
+for(let i = 1; i <= 10; i++) {
+    console.log(`i is : ${i}`);
+    for(let j = 1; j <= 4; j++) {
+        console.log(`   j is : ${j}`);
+    }
+} 
+*/
+
+/* 
+const seatingChart = [
+    ['kristen', 'erik', 'namita'],
+    ['geoffrey', 'juanita', 'antonio', 'kevin'],
+    ['yuma', 'sakura', 'jack', 'erika']
+]
+
+for(let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`row ${i + 1}`)
+    for(let j = 0; j < row.length; j++) {
+        console.log(row[j]);
+    }
+} 
+*/
+
