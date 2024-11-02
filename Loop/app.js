0e552ebe2363ef4c95c698a576b7e4a54478d724
jooho2075 <parkjh2075@icloud.com>
2024-11-02 12:30:57 +0900

    24.11.02_203,204_GuessingGame

--- Loop/app.js
+++ Loop/app.js
@@ -104,3 +104,37 @@ for(let i = 0; i < 1000; i++) {
 }
 console.log("Ok you win!!");
 */
+
+// 202,203_guesing game(추측 게임 만들기)
+let maximum = parseInt(prompt("Enter the maximum number"));
+while(!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt("Enter your first guess(Type 'q' to quit)");
+let attempts = 1;
+
+while(parseInt(guess) !== targetNum) {
+    if(guess === 'q') {
+        break;
+    }
+    guess = parseInt(guess);
+    if(guess > targetNum) {
+        guess = prompt("Too high!");
+        attempts++;
+    } else if(guess < targetNum) {
+        guess = prompt("Too low!");
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit");
+    }
+}
+
+if(guess === 'q') {
+    console.log("Ok quitting!");
+} else {
+    console.log("Congratulation!");
+    console.log(`You got it! It took you ${attempts} guesses`);
+}
