6d53942fbceea19da8f4b834f11eda308e7d9955
jooho2075 <parkjh2075@icloud.com>
2024-11-08 10:57:22 +0900

    24.11.08_Multiple Arguments

--- Function/app.js
+++ Function/app.js
@@ -8,6 +8,21 @@ function singSong() {
 */
 
 // section211 - Argument
+/*
 function greet(firstName) {
     console.log(`firstName is : ${firstName}`);
+}
+*/
+
+// sectioin212 - Multiple Arguments
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}!`);
+}
+
+function repeat(msg, num) {
+    let result = '';
+    for(let i = 0; i< num; i++) {
+        result += msg;
+    }
+    console.log(result);
 }
