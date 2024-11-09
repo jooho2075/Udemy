e4b9559675a04edcc009483f3d5089836fcf9bed
jooho2075 <parkjh2075@icloud.com>
2024-11-09 10:45:28 +0900

    24.11.09_Map

--- Array_Method/app.js
+++ Array_Method/app.js
@@ -12,4 +12,12 @@ numbers.forEach(function(el) {
         console.log(el);    
     }
 })
-    */
+*/
+
+// section227 - Map method
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const doubles = numbers.map(function(num) {
+    return num * 2;
+})
+console.log(doubles);
