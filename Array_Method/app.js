754c436a568ebff563d4322a71fe41a1af1de8a3
jooho2075 <parkjh2075@icloud.com>
2024-11-09 10:35:38 +0900

    24.11.09_ForEach

---
+++ Array_Method/app.js
@@ -0,0 +1,15 @@
+// section226 - forEach Method
+/*
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+function print(element) {
+    console.log(element);
+}
+numbers.forEach(print);
+
+numbers.forEach(function(el) {
+    if(el % 2 == 0) {
+        console.log(el);    
+    }
+})
+    */
