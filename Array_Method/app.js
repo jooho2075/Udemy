86c50677a96d5f21272a9d449aa5173a43a4d631
jooho2075 <parkjh2075@icloud.com>
2024-11-09 11:07:48 +0900

    24.11.09_Arrow Function

--- Array_Method/app.js
+++ Array_Method/app.js
@@ -15,9 +15,31 @@ numbers.forEach(function(el) {
 */
 
 // section227 - Map method
+/*
 const numbers = [1,2,3,4,5,6,7,8,9,10];
 
 const doubles = numbers.map(function(num) {
     return num * 2;
 })
 console.log(doubles);
+*/
+
+// section228 - Arrow function
+/*
+const add = function(x, y) {
+    return x + y;
+}
+
+const add = (x, y) => {
+    return x + y
+}
+
+const square = (num) => {
+    return num * num;
+}
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
+*/
+
