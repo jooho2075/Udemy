3e3198678156196464d95356a8c526aef742cb57
jooho2075 <parkjh2075@icloud.com>
2024-11-15 17:39:17 +0900

    24.11.15_코딩연습53

--- Dom/Manipulating/app.js
+++ Dom/Manipulating/app.js
@@ -7,3 +7,9 @@ for(let link of allLinks) {
 }
 */
 
+// 코딩연습 53 
+const span = document.querySelector("span");
+// document.querySelector 메서드로 "Delicious"가 포함된 텍스트 요소 선택
+span.innerText = 'Disgusting';
+// 변수에서 할당 연산자=과 함께 .innerText 속성을 사용해서 
+// 요소의 텍스트를 "Disgusting"으로 변환
