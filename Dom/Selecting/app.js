a8f5748efa708fe334f71dfed97c9383654a2cde
jooho2075 <parkjh2075@icloud.com>
2024-11-15 16:59:58 +0900

    24.11.15_querySelector/querySelectorAll

--- Dom/Selecting/app.js
+++ Dom/Selecting/app.js
@@ -1,4 +1,5 @@
 // class249 - getElementsByTagName, className
+/*
 const allImages = document.getElementsByTagName('img');
 
 for(let img of allImages) {
@@ -8,4 +9,11 @@ for(let img of allImages) {
 const squareImages = docuement.getElementsByClassName('square');
 for(let img of squareImages) {
     img.src = '';
+}
+*/
+
+// class250 - querySelector/querySelectorAll
+const links = document.querySelectorAll('p a');
+for(let link of links) {
+    console.log(link.href); // 모든 <a>tag 출처가 출력됨
 }
