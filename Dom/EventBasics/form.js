29a5684e87061a88d331730588175bc31e272a56
Juho Park <parkjh2075@icloud.com>
2024-12-18 18:16:07 +0900

    24.12.18_268.preventDefault 연습2

--- Dom/EventBasics/form.js
+++ Dom/EventBasics/form.js
@@ -1,5 +1,15 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
+
 form.addEventListener("submit", function(e) {
     e.preventDefault(); // e.preventDefault - 특정 event의 기본 동작이 수행되지 않도록 막아주는 역할
-    console.log("submitted the form");
+    
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    
+    newLI.innerText = catName;
+    list.append(newLI);
+
+    input.value = "";
 });
