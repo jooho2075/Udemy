8b1257e83ed7f30cc2854a0f9d38869f913e81ca
Juho Park <parkjh2075@icloud.com>
2024-12-18 16:04:13 +0900

    24.12.18_263.addEventListener

--- Dom/EventBasics/app.js
+++ Dom/EventBasics/app.js
@@ -14,3 +14,27 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked h1');
 }
+
+ // 263.addEventListener
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function() {
+    alert("Clicked");
+})
+
+function twist() {
+    console.log("Twist");
+}
+
+function shout() {
+    console.log("shout");
+}
+
+const tasButton = document.querySelector("#tas");
+
+/*
+tasButton.onclick = twist;
+tasButton.onclick = shout; // 결과 : shout만 나옴 -> 하나 이상은 나올 수 없음
+*/
+
+tasButton.addEventListener('click', twist, { once: true }); // 이 콜백은 해당 이벤트가 발생할 때 처음 한 번 실행됨
+tasButton.addEventListener('click', shout);
