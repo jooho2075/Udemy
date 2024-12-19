292399d1f918af08e87bc9c81b7526a5a7a71b80
jooho2075 <parkjh2075@icloud.com>
2024-12-19 18:44:55 +0900

    24.12.19_280.Callback

--- Async_JS/app.js
+++ Async_JS/app.js
@@ -1,6 +1,44 @@
 // 279.WebAPI와 단일 스레드
+/*
 console.log("Sending request to server");
 setTimeout(() => {
     console.log("here is your data from the server");
 }, 3000)
 console.log("I am at the end of the file");
+*/
+
+// 280.Callback
+/*
+setTimeout(() => {
+    document.body.style.backgroundColor = 'red';
+    setTimeout(() => {
+        document.body.style.backgroundColor = 'orange';
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'yellow';
+            setTimeout(() => {
+                document.body.style.backgroundColor = 'green';
+                setTimeout(() => {
+                    document.body.style.backgroundColor = 'blue';
+                    setTimeout(() => {
+                        document.body.style.backgroundColor = 'skyblue';
+                        setTimeout(() => {
+                            document.body.style.backgroundColor = 'purple';
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+*/
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay)
+}
+
+delayedColorChange('olive', 3000, () => {
+    console.log("Inside the callback");
+});
