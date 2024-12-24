3e754c8d66a245f1b22106ff4abf731dfa8145f1
jooho2075 <parkjh2075@icloud.com>
2024-12-24 20:45:54 +0900

    24.12.24_팩토리 함수

--- Prototype_Class_OOP/app.js
+++ Prototype_Class_OOP/app.js
@@ -1,4 +1,5 @@
 // 303.Prototype
+/*
 String.prototype.yell = function() {
     return `${this.toUpperCase()}`
 };
@@ -6,3 +7,32 @@ String.prototype.yell = function() {
 Array.prototype.pop = function() {
     return "sorry I want that element, I will never pop it up";
 }
+*/
+
+// 305.Factory function
+function hex(r,g,b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function rgb(r,g,b) {
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function makeColor(r,g,b) { // 이 함수가 객체를 만들어 주는 역할 
+    const color = {}
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function() {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function() {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+
+const firstColor = makeColor(35,255,150);
+//hex(255, 100, 25); // 16진수 색상을 다시 가져올 것
