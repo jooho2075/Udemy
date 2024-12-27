4dc67e294533c3b7392f958b7822303edbdff5b8
jooho2075 <parkjh2075@icloud.com>
2024-12-27 14:17:03 +0900

    24.12.27_333.디렉토리의 필요성

--- Modules_NPM/app.js
+++ Modules_NPM/app.js
@@ -1,3 +1,5 @@
+// 332
+/*
 const math = require ('./math');
 
 // const { PI, square } = require('./math'); // PI랑 square만 가져오도록 구조 분해한 것
@@ -6,3 +8,9 @@ console.log(math); // 결과 : 빈 객체
 console.log(math.add(1,2));
 console.log(math.PI);
 console.log(math.square(9));
+*/
+
+// 333. 디렉토리의 필요성
+const cats = require('./CatShelter')
+
+console.log("Required an entire directory", cats);
