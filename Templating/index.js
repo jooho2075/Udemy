6c9f7f0c5af0d91466ec7d92ddca7d18210b76c1
jooho2075 <parkjh2075@icloud.com>
2024-12-30 13:16:38 +0900

    24.12.30_EJS 보간 구문

--- Templating/index.js
+++ Templating/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 // EJS의 사용을 알리는 부분(2개의 인수 사용 : view engine, ejf)
 app.set('view engine', 'ejs');
+// index.js파일이 있는 현재 디렉토리를 가져와 그곳에 도달하기 위한 전체 디렉토리에 /views를 붙여주는 역할
 app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     //res.send("Hi");
