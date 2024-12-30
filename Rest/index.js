679c015f079e9c79f0408cd38ccf92768e715b0b
jooho2075 <parkjh2075@icloud.com>
2024-12-30 21:05:30 +0900

    24.12.30_요청 구문 분석하기

--- Rest/index.js
+++ Rest/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+// GitBash 결과 : { meat: 'pork', quantity: '1' }
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("on port 3000");
 });
@@ -10,5 +14,7 @@ app.get('/tacos', (req, res) => {
 });
 
 app.post('/tacos', (req, res) => {
-    res.send("POST /tacos response");
+    //console.log(req.body); // GitBash 결과 : undefined
+    const { meat, quantity } = req.body;
+    res.send(`Ok, here are your ${quantity} ${meat} tacos`);
 });
