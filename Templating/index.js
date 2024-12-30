1f4fc5b139a461f3d1403a14efbfe4c603d86178
jooho2075 <parkjh2075@icloud.com>
2024-12-30 13:05:59 +0900

    24.12.31_View directory 설정하기

--- Templating/index.js
+++ Templating/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // EJS의 사용을 알리는 부분(2개의 인수 사용 : view engine, ejf)
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     //res.send("Hi");
     res.render('home.ejs');
