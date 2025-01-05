8795387b61f54387a972b2551b35dc9c255f69c5
jooho2075 <parkjh2075@icloud.com>
2025-01-05 16:39:37 +0900

    25.01.05_417.카테고리별 필터링

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -24,8 +24,14 @@ const categories = ['fruit', 'vegetable', 'dairy'];
 
 
 app.get('/products', async (req, res) => { // 라우트를 위한 비동기 콜백 패턴
-    const products = await Product.find({});
-    res.render('products/index', { products });
+    const { category } = req.query;
+    if(category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
 });
 
 app.get('/products/new', (req, res) => {
