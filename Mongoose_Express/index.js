4f7984e7180475eb04d46a9687fcf4456dcb001e
jooho2075 <parkjh2075@icloud.com>
2025-01-05 16:15:48 +0900

    25.01.05_415.카테고리 셀렉터 위의 탄젠트

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -20,13 +20,16 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+
 app.get('/products', async (req, res) => { // 라우트를 위한 비동기 콜백 패턴
     const products = await Product.find({});
     res.render('products/index', { products });
 });
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 });
 
 app.post('/products', async(req, res) => {
@@ -44,7 +47,7 @@ app.get('/products/:id', async(req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product });
+    res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async(req, res) => { // 상품 업데이트 로직
