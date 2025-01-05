14f5e2345196c7b7cc94c3e4e53922a428e28511
jooho2075 <parkjh2075@icloud.com>
2025-01-05 12:56:07 +0900

    25.01.05_413.프로덕트 만들기

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -16,10 +16,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand') // 주소는 localhost �
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/products', async (req, res) => { // 라우트를 위한 비동기 콜백 패턴
     const products = await Product.find({});
     res.render('products/index', { products });
+});
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+});
+
+app.post('/products', async(req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save(); // 상품 저장 코드
+    res.redirect(`/products/${newProduct._id}`); // DB에 완전 저장하도록 하는 코드
 });
 
 app.get('/products/:id', async(req, res) => {
