8692ea44e9824af688f88bf0338368139db21f39
jooho2075 <parkjh2075@icloud.com>
2025-01-08 23:31:19 +0900

    25.01.08_619.배열 및 오브젝트 전달

--- First_React/first-react-app/src/App.jsx
+++ First_React/first-react-app/src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
-import Greeter from "./Greeter";
-import Die from "./Die";
+// import Greeter from "./Greeter";
+// import Die from "./Die";
+import ListPicker from './ListPicker';
 
 function App() {
   return (
     <div>
-      
+      {/*
       <Greeter person="Park" from="Colt" />
       <Greeter />
       <Greeter person="Lee" from="Colt" /> 
      
       <Die numSides={20}/>
       <Die />
-      <Die numSides={10}/>
+      <Die numSides={10}/> 
+      */}
+      <ListPicker values={[1,2,3,4,5]}/>
+      <ListPicker values={["a", "b", "c"]}/>
     </div>
   );
 }
