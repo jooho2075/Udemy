ff192baf6fd4fd44127f1aadca3336f03d123b3c
jooho2075 <parkjh2075@icloud.com>
2025-01-13 20:50:01 +0900

    25.01.13_625.Key Property

--- First_React/first-react-app/src/App.jsx
+++ First_React/first-react-app/src/App.jsx
@@ -9,10 +9,10 @@ import './App.css'
 import ShoppingList from './ShoppingList';
 
 const data = [
-  {item: 'eggs', quantity: 12, completed: false},
-  {item: 'milk', quantity: 1, completed: true},
-  {item: 'chicken breast', quantity: 4, completed: false},
-  {item: 'carrot', quantity: 6, completed: true},
+  {id: 1, item: 'eggs', quantity: 12, completed: false},
+  {id: 2, item: 'milk', quantity: 1, completed: true},
+  {id: 3, item: 'chicken breast', quantity: 4, completed: false},
+  {id: 4, item: 'carrot', quantity: 6, completed: true},
 ]
 
 function App() {
--- First_React/first-react-app/src/ShoppingList.jsx
+++ First_React/first-react-app/src/ShoppingList.jsx
@@ -2,10 +2,12 @@ function ShoppingList({items}) {
     return(
         <ul>
             {items.map((i) => (
-                <li style={{
-                    color: i.completed ? "grey" : "red",
-                    textDecoration: i.completed ? "line-through" : "none"}}>
-                    {i.item} - {i.quantity}
+                <li 
+                    key={i.id}
+                    style={{
+                        color: i.completed ? "grey" : "red",
+                        textDecoration: i.completed ? "line-through" : "none"}}>
+                        {i.item} - {i.quantity}
                 </li>
             ))}
         </ul>
