1633d3a0ec0ecf4d91fb45fff82886328a2723b5
jooho2075 <parkjh2075@icloud.com>
2025-01-14 23:09:40 +0900

    25.01.14_onMouseHover Event

--- First_React/first-react-app/src/Clicker.jsx
+++ First_React/first-react-app/src/Clicker.jsx
@@ -2,11 +2,16 @@ function handleClick() {
     console.log("Clicked the Button");
 }
 
+function handleHover() {
+    console.log("Hover the Mosue");
+}
+
 function Clicker() {
     return(
         <div>
             <p>Click the Button</p>
             <button onClick={handleClick}>Click</button>
+            <p onMouseOver={handleHover}>Hover Over Me</p>
         </div>
     );
 }
