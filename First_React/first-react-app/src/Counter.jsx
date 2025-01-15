f64dc31ebc0c0935b6b93a052ef43d5391d7c289
jooho2075 <parkjh2075@icloud.com>
2025-01-15 22:10:10 +0900

    25.01.15_useState() 훅 사용

--- First_React/first-react-app/src/Counter.jsx
+++ First_React/first-react-app/src/Counter.jsx
@@ -1,14 +1,14 @@
+import { useState } from "react";
+
 function Counter() {
-    let num = 0;
-    const incrementNum = () => {
-        num += 1
-        console.log(num);
-    };
-    
+    const [num, setNum] = useState(5);
+    const changeNum = () => {
+        setNum(num + 1);
+    }
     return (
         <div>
             <p>The Count is : {num}</p>
-            <button onClick={incrementNum}>Increment</button>
+            <button onClick={changeNum}>Increment</button>
         </div>
     );
 }
