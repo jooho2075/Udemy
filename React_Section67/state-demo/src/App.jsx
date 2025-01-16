1bb9235718c7abe20f6e402b84d50e70244a4a9d
jooho2075 <parkjh2075@icloud.com>
2025-01-16 23:25:41 +0900

    25.01.16_645.상태에 Object 사용

--- React_Section67/state-demo/src/App.jsx
+++ React_Section67/state-demo/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
-import Counter from './Counter';
+// import Counter from './Counter';
+import ScoreKeeper from './ScoreKeeper';
 // import Dumbo from './Dumbo';
 
 function App() {
   return (
     <>
     <h1>State Demo</h1>
-    <Counter />
+    <ScoreKeeper/>
+
+    {/* <Counter /> */}
 
     {/* <Dumbo /> */}
     </>
---
+++ React_Section67/state-demo/src/ScoreKeeper.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function ScoreKeeper() {
+    const [scores, setScores] = useState({p1Score: 0, p2Score: 0});
+    
+    function increaseP1Score() {
+        setScores((oldScores) => {
+            return {...oldScores, p1Score: oldScores.p1Score + 1 }
+        });
+    };
+
+    function increaseP2Score() {
+        setScores((oldScores) => {
+            return {...oldScores, p2Score: oldScores.p2Score + 1 }
+        });
+    };
+
+    return (
+        <div>
+            <p>Player1 : {scores.p1Score}</p>
+            <p>Player2 : {scores.p2Score}</p>
+            <button onClick={increaseP1Score}>+1 Player1</button>
+            <button onClick={increaseP2Score}>+1 Player2</button>
+        </div>
+    );
+}
