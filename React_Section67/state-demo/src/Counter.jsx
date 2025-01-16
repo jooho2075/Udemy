27a6040ac09f9ce3c47cbea1b9d368bf465a3c04
jooho2075 <parkjh2075@icloud.com>
2025-01-16 22:07:49 +0900

    25.01.16_642.업데이트 함수로 상태 설정

---
+++ React_Section67/state-demo/src/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const addOne = () => {
+        setCount(count + 1);
+    };
+    const addThree = () => {
+        // 아래 callBack 문법을 사용하면
+        // 이전 또는 현재 값에 의존하는 상태를 업데이트할 수 있음
+        setCount((currentCount) => currentCount + 1); // React가 자동으로 현재 상태 값을 이 매개 변수로 전달함
+        console.log(count);
+        setCount((currentCount) => currentCount + 1);
+        setCount((currentCount) => currentCount + 1);
+    };
+    return (
+        <div>
+            <p>Count : { count }</p>
+            <button onClick={addOne}>+1</button>
+            <button onClick={addThree}>+3</button>
+        </div>
+    );
+}
+
+export default Counter;
