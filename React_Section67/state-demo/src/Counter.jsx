bd7c72ed99c06fb1ac0cbb7663de40e1e6a6227e
jooho2075 <parkjh2075@icloud.com>
2025-01-16 22:34:57 +0900

    25.01.16_React에서 다시 랜더링 하는 이유

--- React_Section67/state-demo/src/Counter.jsx
+++ React_Section67/state-demo/src/Counter.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 function Counter() {
+    console.log("Re-Rendered");
+    
     const [count, setCount] = useState(0);
     const addOne = () => {
         setCount(count + 1);
     };
+    const setToTen = () => {
+        setCount(10);
+    }
     const addThree = () => {
         // 아래 callBack 문법을 사용하면
         // 이전 또는 현재 값에 의존하는 상태를 업데이트할 수 있음
@@ -18,6 +23,7 @@ function Counter() {
             <p>Count : { count }</p>
             <button onClick={addOne}>+1</button>
             <button onClick={addThree}>+3</button>
+            <button onClick={setToTen}>Set to Ten</button>
         </div>
     );
 }
