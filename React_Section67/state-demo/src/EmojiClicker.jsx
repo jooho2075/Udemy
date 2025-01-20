63a82cec552b4a2c8204537a9df59e4ff076385b
jooho2075 <parkjh2075@icloud.com>
2025-01-20 21:05:14 +0900

    25.01.20_UUID로 ID 생성

--- React_Section67/state-demo/src/EmojiClicker.jsx
+++ React_Section67/state-demo/src/EmojiClicker.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { v4 as uuid } from 'uuid';
 
 export default function EmojiClicker() {
-    const [emojies, setEmojies] = useState([":("]);
+    const [emojies, setEmojies] = useState([{ id: uuid(), emoji: ":(" }]); // 이 곳에 무작위 id 생성
     const addEmoji = () => {
-        setEmojies(oldEmojies => [...oldEmojies, ":)"]);
+        setEmojies(oldEmojies => [...oldEmojies, { id: uuid(), emoji: ":)" }]);
     };
     return (
         <div>
             {emojies.map((e) => (
-                <span style={{ fontSize: "4rem" }}>{e}</span>
+                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji }>Add Emoji</button>
         </div>
