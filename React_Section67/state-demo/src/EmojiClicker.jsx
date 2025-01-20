f2dc462d15e6984c5218cc78817ce4a4880cbc95
jooho2075 <parkjh2075@icloud.com>
2025-01-20 22:02:11 +0900

    25.01.20_React에서 배열의 요소를 삭제하는 방법

--- React_Section67/state-demo/src/EmojiClicker.jsx
+++ React_Section67/state-demo/src/EmojiClicker.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import { v4 as uuid } from 'uuid';
 
+function randomEmoji() {
+    const choices = ["A", "B", "C", "D", "E", "F"];
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
 export default function EmojiClicker() {
-    const [emojies, setEmojies] = useState([{ id: uuid(), emoji: ":(" }]); // 이 곳에 무작위 id 생성
+    const [emojies, setEmojies] = useState([{ id: uuid(), emoji: randomEmoji() }]); // 이 곳에 무작위 id 생성
     const addEmoji = () => {
-        setEmojies(oldEmojies => [...oldEmojies, { id: uuid(), emoji: ":)" }]);
+        setEmojies(oldEmojies => [...oldEmojies, { id: uuid(), emoji: randomEmoji() }]);
+    };
+    const deleteEmoji = (id) => {
+        // 단계1.delete the emoji with the specified id(특정 아이디로 Emoji 삭제)
+        setEmojies(prevEmojies => {
+            return prevEmojies.filter(e => e.id !== id);    
+        })
     };
     return (
         <div>
             {emojies.map((e) => (
-                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
+                // 단계2. deleteEmoji를 호출할 때 이 곳에 ID 전달해야 함
+                <span 
+                    onClick={() => deleteEmoji(e.id)} 
+                    key={e.id} 
+                    style={{ fontSize: "4rem" }}>
+                        {e.emoji}
+                </span>
             ))}
             <button onClick={addEmoji }>Add Emoji</button>
         </div>
