d10acdf123c97310bc1d7bbc1ebfd17d17f637ae
jooho2075 <parkjh2075@icloud.com>
2025-02-04 22:36:45 +0900

    25.02.04_Section68 : BoxGrid Practice

---
+++ React_Section68/box-game/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+// import Box from './Box';
+import BoxGrid from './BoxGrid';
+
+function App() {
+  return(
+    <>
+      {/* 
+      <Box />
+      <Box />
+      <Box /> 
+      */}
+      <BoxGrid />
+    </>
+  )
+}
+
+export default App
---
+++ React_Section68/box-game/src/Box.jsx
@@ -0,0 +1,14 @@
+import "./Box.css";
+import { useState } from "react";
+
+function Box({isActive, toggle}) {
+    return (
+        <div
+            onClick={toggle}
+            className="Box" 
+            style={{ backgroundColor: isActive? "red" : "black"}}
+        ></div>
+    );
+}
+
+export default Box;
