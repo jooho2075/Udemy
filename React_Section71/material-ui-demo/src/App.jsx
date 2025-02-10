17a1a9b3a11de999d40f731cea5a32e433dc20f6
jooho2075 <parkjh2075@icloud.com>
2025-02-10 16:10:17 +0900

    25.02.10_677.Material UI 설치

---
+++ React_Section71/material-ui-demo/src/App.jsx
@@ -0,0 +1,20 @@
+import { IconButton } from '@mui/material';
+import './App.css'
+import Button from '@mui/material/Button';
+import AlarmIcon from '@mui/icons-material/Alarm';
+
+function App() {
+  return (
+    <div>
+      <Button size="small" variant="contained" onClick={() => alert("Hi!!")}>Contained</Button>
+      <Button variant="text">Text</Button>
+      <Button color="success" variant="outlined">Outlined</Button><br />
+
+      <IconButton color="secondary" aria-label="add an alartm">
+        <AlarmIcon />
+      </IconButton>
+    </div>
+  );
+}
+
+export default App
