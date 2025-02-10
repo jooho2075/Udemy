d0d7003007c8fec8d406f046b91124fd1c82f3da
jooho2075 <parkjh2075@icloud.com>
2025-02-10 16:23:32 +0900

    25.02.10_678.Rating Component

--- React_Section71/material-ui-demo/src/App.jsx
+++ React_Section71/material-ui-demo/src/App.jsx
@@ -1,18 +1,23 @@
-import { IconButton } from '@mui/material';
+// import { IconButton } from '@mui/material';
 import './App.css'
-import Button from '@mui/material/Button';
-import AlarmIcon from '@mui/icons-material/Alarm';
+// import Button from '@mui/material/Button';
+// import AlarmIcon from '@mui/icons-material/Alarm';
+import RatingDemo from './RatingDemo';
 
 function App() {
   return (
     <div>
+      {/* 
       <Button size="small" variant="contained" onClick={() => alert("Hi!!")}>Contained</Button>
       <Button variant="text">Text</Button>
       <Button color="success" variant="outlined">Outlined</Button><br />
 
       <IconButton color="secondary" aria-label="add an alartm">
         <AlarmIcon />
-      </IconButton>
+      </IconButton><br /> 
+      */}
+
+      <RatingDemo />
     </div>
   );
 }
