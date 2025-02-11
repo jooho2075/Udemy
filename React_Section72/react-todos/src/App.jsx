c37cfbc93bdb3a49bd587750de3f2c00be60327c
jooho2075 <parkjh2075@icloud.com>
2025-02-11 13:36:15 +0900

    25.02.11_682.할 일 목록 컴포넌트

--- React_Section72/react-todos/src/App.jsx
+++ React_Section72/react-todos/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import CssBaseline from "@mui/material/CSSBaseline";
 import './App.css'
+import TodoList from "./TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <CssBaseline />
       <h1>Todos</h1>
+      <TodoList />
     </>
   )
 }
