32bcdeb6b71bdf39dcd1f28307efc6a4e84163c8
jooho2075 <parkjh2075@icloud.com>
2025-02-11 14:59:16 +0900

    25.02.11_686.LocalStorage 추가

--- React_Section72/react-todos/src/TodoList.jsx
+++ React_Section72/react-todos/src/TodoList.jsx
@@ -1,17 +1,28 @@
 import List from '@mui/material/List';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 
-const initialTodos = [
-    {id: 1, text: "walk the dog", completed: false},
-    {id: 2, text: "walk the cat", completed: false},
-    {id: 3, text: "walk the fish", completed: true},
-    {id: 4, text: "walk the chicken", completed: false},
-]
+const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+
+    if(!data) return [];
+    return data;
+};
+
+// const initialTodos = [
+//     {id: 1, text: "walk the dog", completed: false},
+//     {id: 2, text: "walk the cat", completed: false},
+//     {id: 3, text: "walk the fish", completed: true},
+//     {id: 4, text: "walk the chicken", completed: false},
+// ]
 
 function TodoList() {
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState(getInitialData);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     const removeTodo = (id) => {
         setTodos((prevTodos) => {
