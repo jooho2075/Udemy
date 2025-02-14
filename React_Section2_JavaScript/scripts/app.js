5c6c763ff9afe5e596283f6fa5297cfe8cbca7bf
jooho2075 <parkjh2075@icloud.com>
2025-02-14 15:03:34 +0900

    25.02.14_import/export

---
+++ React_Section2_JavaScript/scripts/app.js
@@ -0,0 +1,5 @@
+// import { apiKey } from "./util";
+
+import apiKey from "./util.js"; // 위와 같은 의미
+
+console.log(apiKey);
---
+++ React_Section2_JavaScript/scripts/util.js
@@ -0,0 +1,5 @@
+// export를 해주면 이 변수를 다른 파일에서도 사용이 가능함
+// export let apiKey = "adnasdoasflak1";
+
+export default "adnasdoasflak1"; // 위와 같은 의미
+
