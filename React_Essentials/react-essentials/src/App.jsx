074dfd5999abfccd007f756e35fb2ac3133fe20a
jooho2075 <parkjh2075@icloud.com>
2025-02-17 16:42:10 +0900

    25.02.17_동적값 출력 및 활용

--- React_Essentials/react-essentials/src/App.jsx
+++ React_Essentials/react-essentials/src/App.jsx
@@ -1,16 +1,30 @@
 import './App.css'
 
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+// 동적값 출력 및 활용
+function Header() {
+    const description = reactDescription[getRandomInt(2)];
+
+    return (
+    <header>
+        <h1>React Essentials</h1>
+        <p>
+            {description} Reacct concepts you will need for almost any app you are
+            going to build!
+        </p>
+    </header>
+    );
+}
+
 function App() {
     return (
         <div>
-            <header>
-                <h1>React Essentials</h1>
-                <p>
-                    Fundamental Reacct concepts you will need for almost any app you are
-                    going to build!
-                </p>
-            </header>
-
+            <Header></Header>
             <main>
                 <h2>Time to get started</h2>
             </main>
--- React_Essentials/react-essentials/src/index.jsx
+++ React_Essentials/react-essentials/src/index.jsx
@@ -1,4 +1,7 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; 
+// 이 JSX 코드는 함수로 전환이 되지 않는 것
+// 값으로 사용되고 있음
+// 호출된 다른 메소드의 인수로 사용됨
 
 import App from "./App.jsx";
 import "./index.css";
