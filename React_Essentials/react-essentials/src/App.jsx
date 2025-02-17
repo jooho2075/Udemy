b10f79327f336ee843b3f5f727e2e8cb0ce43913
jooho2075 <parkjh2075@icloud.com>
2025-02-17 16:09:06 +0900

    24.02.17_Section3.리엑트 핵심 초기 프로젝트 구성

---
+++ React_Essentials/react-essentials/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <header>
+                <h1>React Essentials</h1>
+                <p>
+                    Fundamental Reacct concepts you will need for almost any app you are
+                    going to build!
+                </p>
+            </header>
+
+            <main>
+                <h2>Time to get started</h2>
+            </main>
+        </div>
+    );
+}
+
+export default App
