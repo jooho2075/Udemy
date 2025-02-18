278757903ec13b05652664bec27fdd26d69f5d6c
jooho2075 <parkjh2075@icloud.com>
2025-02-18 19:20:48 +0900

    25.02.18_data기반 state(상태) 가져오기 및 출력

--- React_Essentials/react-essentials/src/App.jsx
+++ React_Essentials/react-essentials/src/App.jsx
@@ -6,9 +6,10 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+    const [selectedTopic, setSelectedTopic] = useState('components');
 
     const handleSelect = (selectedButton) => {
         // selectedButton => 'component', 'jsx', 'props', 'state' : selectedButton이라는 매개변수에서 받는 4개의 변수
@@ -48,7 +49,15 @@ function App() {
                         {/* 위와 동일한 방법*/}
                         {/* <TabButton label='Components' /> */}
                     </menu>
-                    {selectedTopic};
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>
+                            {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
