d106ef0c98378207d0502abd83f6ff76ec600587
jooho2075 <parkjh2075@icloud.com>
2025-02-19 16:18:37 +0900

    25.02.19_기본 Prop(속성)값 설정

--- React_Essentials/react-essentials/src/components/Tabs.jsx
+++ React_Essentials/react-essentials/src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-function Tabs({ children, buttons, ButtonsContainer }) {
+function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
     // const ButtonsContainer = buttonsContainer;
     return (
         <>
