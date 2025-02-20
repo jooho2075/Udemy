dd60ae47a5a64be1b7f44a836b8e563a9978ae8c
jooho2075 <parkjh2075@icloud.com>
2025-02-20 14:47:09 +0900

    25.02.20_사용자 입력 및 양방향 바인디이

--- React_TicTacToc/tictactoc-game/src/components/Player.jsx
+++ React_TicTacToc/tictactoc-game/src/components/Player.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
-function Player({name, symbol}) {
+function Player({initialName, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     const handleEditClick = () => {
-        setIsEditing(editing => !editing); // => schedules a state update to true
-        setIsEditing(editing => !editing); // => schedules a state update to true
+        setIsEditing((editing) => !editing); // => schedules a state update to true
     };
 
-    let playerName = <span className="player-name">{name}</span>;
+    const handleChange = (event) => {
+        setPlayerName(event.target.value);
+    };
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     // let btnCaption = 'Edit';
+    
     if(isEditing === true) {
-        playerName = <input type="text" required value={name}></input>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}></input>
         // btnCaption = 'Save';
     }
 
     return (
         <li>
             <span className="playe-info">
-              {playerName}
+              {editablePlayerName}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
