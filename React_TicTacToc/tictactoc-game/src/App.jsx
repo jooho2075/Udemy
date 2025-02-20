1f7db965f0018876c1d11425a3e17538a062abeb
jooho2075 <parkjh2075@icloud.com>
2025-02-20 13:33:58 +0900

    25.02.20_tictactoe 첫단계

--- React_TicTacToc/tictactoc-game/src/App.jsx
+++ React_TicTacToc/tictactoc-game/src/App.jsx
@@ -1,6 +1,19 @@
 function App() {
   return (
-    <p>Coming soon...</p>  
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
   );
 }
 
