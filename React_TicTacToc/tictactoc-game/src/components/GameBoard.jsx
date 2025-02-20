a828c7b4069970f778925b44751c033d991054da
jooho2075 <parkjh2075@icloud.com>
2025-02-20 15:22:26 +0900

    25.02.20_immutable object state(불변의 객체 상태)로 업데이트 하기

--- React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
+++ React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,26 @@ const initialGameBoard = [
 ];
 
 function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            // memory에 저장되는 새로운 배열 객체
+            // 기존 배열 요소를 자식 요소로 저장하고 있음
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; 
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    };
+
     return(
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button>{playerSymbol}</button>
+                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>
                         ))}
                     </ol>
