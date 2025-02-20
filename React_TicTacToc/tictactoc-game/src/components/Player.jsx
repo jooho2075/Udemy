d99c76ae3c0f0fbdc963af8e9fe5d4d4fe51b0cb
jooho2075 <parkjh2075@icloud.com>
2025-02-20 13:55:34 +0900

    25.02.20_State활용 : Player name

--- React_TicTacToc/tictactoc-game/src/components/Player.jsx
+++ React_TicTacToc/tictactoc-game/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing(true); // button을 클릭한 시점이기 때문에 true로 설정
+    };
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if(isEditing === true) {
+        playerName = <input type="text" required></input>
+    }
+
     return (
         <li>
             <span className="playe-info">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
