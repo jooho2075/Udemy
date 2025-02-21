837e1b9a433db0551680c867a33bb70680233d58
jooho2075 <parkjh2075@icloud.com>
2025-02-21 20:18:05 +0900

    25.02.21_State 끌어올리기 대안

--- React_TicTacToc/tictactoc-game/src/App.jsx
+++ React_TicTacToc/tictactoc-game/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol && 
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -82,7 +82,7 @@ function App() {
     setPlayers(prevPlayers => {
       return {
         ...prevPlayers,
-        [symbol]
+        [symbol]: newName
       };
     });
   };
@@ -95,11 +95,13 @@ function App() {
             initialName="Player 1" 
             symbol="X" 
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player 
             initialName="Player 2" 
             symbol="O" 
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
--- React_TicTacToc/tictactoc-game/src/components/Player.jsx
+++ React_TicTacToc/tictactoc-game/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Player({initialName, symbol, isActive}) {
+function Player({initialName, symbol, isActive, onChangeName}) {
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName);
 
@@ -10,6 +10,10 @@ function Player({initialName, symbol, isActive}) {
 
     const handleChange = (event) => {
         setPlayerName(event.target.value);
+
+        if(isEditing) {
+            onChangeName(symbol, playerName);
+        }
     };
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
