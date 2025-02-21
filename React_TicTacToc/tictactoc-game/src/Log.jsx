a5a1d2437768f5ac61c407618a3c50bf38cf311e
jooho2075 <parkjh2075@icloud.com>
2025-02-21 18:35:39 +0900

    25.02.21_State 관리

---
+++ React_TicTacToc/tictactoc-game/src/Log.jsx
@@ -0,0 +1,10 @@
+function Log() { // Log component는 순서가 있는 목록(ol)을 반환할 예정
+    
+    return (
+        <ol id="log">
+
+        </ol>
+    );
+}
+
+export default Log;
--- React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
+++ React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-function GameBoard({onSelectSquare, activePlayerSymbol}) {
+function GameBoard({onSelectSquare, turns}) {
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    /*
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     const handleSelectSquare = (rowIndex, colIndex) => {
@@ -20,6 +28,7 @@ function GameBoard({onSelectSquare, activePlayerSymbol}) {
 
         onSelectSquare();
     };
+    */
 
     return(
         <ol id="game-board">
@@ -28,7 +37,7 @@ function GameBoard({onSelectSquare, activePlayerSymbol}) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>
                         ))}
                     </ol>
