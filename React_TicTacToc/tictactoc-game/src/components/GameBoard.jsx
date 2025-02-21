d8a000deca0dc5f6fe9eaee01b1fbf67dc6c1a1b
jooho2075 <parkjh2075@icloud.com>
2025-02-21 19:33:04 +0900

    25.02.21_계산된 값에서 새로 계산된 값 파생하기

--- React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
+++ React_TicTacToc/tictactoc-game/src/components/GameBoard.jsx
@@ -1,19 +1,4 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-function GameBoard({onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard;
-
-    for(const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
+function GameBoard({onSelectSquare, board}) {
     /*
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -32,7 +17,7 @@ function GameBoard({onSelectSquare, turns}) {
 
     return(
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
