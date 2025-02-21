677e49c12827ea501e952eadf3e765192d7d455d
jooho2075 <parkjh2075@icloud.com>
2025-02-21 18:45:31 +0900

    25.02.21_Component 간의 State 공유

--- React_TicTacToc/tictactoc-game/src/Log.jsx
+++ React_TicTacToc/tictactoc-game/src/Log.jsx
@@ -1,8 +1,12 @@
-function Log() { // Log component는 순서가 있는 목록(ol)을 반환할 예정
-    
+function Log({turns}) { // Log component는 순서가 있는 목록(ol)을 반환할 예정
+
     return (
         <ol id="log">
-
+            {turns.map((turn) => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row}, {turn.square.col}
+                </li>
+            ))}
         </ol>
     );
 }
