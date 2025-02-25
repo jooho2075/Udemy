336054475bc48bf5be1e5d1e7acbd2f0ec3467f2
jooho2075 <parkjh2075@icloud.com>
2025-02-25 14:21:51 +0900

    25.02.25_Event Handling and 양방향 Binding 활용

--- React_Investment_Calculator/investment-calculator/src/components/UserInput.jsx
+++ React_Investment_Calculator/investment-calculator/src/components/UserInput.jsx
@@ -1,24 +1,62 @@
+import { useState } from "react";
+
 function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 1000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    }); // useState에 할당된 초기 상태
+
+     const handleChange = (inputIdentifier, newValue) => { // input 태그와 연결시키는 함수
+        setUserInput(prevUserInput => {
+            return{
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            };
+        });
+     };
+
     return (
         <section id="user-input">
             <div className="input-group">
                 <p>
-                    <label htmlFor="">Initial Investment</label>
-                    <input type="number" required/>
+                    <label>Initial Investment</label>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.initialInvestment} 
+                        onChange={(event) => handleChange('initialInvestment', event.target.value)} 
+                    />
                 </p>
                 <p>
-                    <label htmlFor="">Annual Investment</label>
-                    <input type="number" required/>
+                    <label>Annual Investment</label>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.annualInvestment}
+                        onChange={(event) => handleChange('annualInvestment', event.target.value)}
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
-                    <label htmlFor="">Expected Return</label>
-                    <input type="number" required/>
+                    <label>Expected Return</label>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.expectedReturn}
+                        onChange={(event) => handleChange('ExpectedReturn', event.target.value)}
+                    />
                 </p>
                 <p>
-                    <label htmlFor="">Duration</label>
-                    <input type="number" required/>
+                    <label>Duration</label>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.duration}
+                        onChange={(event) => handleChange('duration', event.target.value)}
+                    />
                 </p>
             </div>
         </section>
