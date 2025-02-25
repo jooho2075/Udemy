9224769cb94a281678b1fab7d6e45e92fcc901a2
jooho2075 <parkjh2075@icloud.com>
2025-02-25 22:37:25 +0900

    25.02.25_Styled Components 소개

--- React_Styling/styling/src/components/AuthInputs.jsx
+++ React_Styling/styling/src/components/AuthInputs.jsx
@@ -1,4 +1,17 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+// div를 개별 component로 만들고 어떤 스타일이든 개발자가 적용하고 싶은
+// 스타일을 가지는 component로 만듬
+
+// tagged template 형식 : 함수 같은 것으로 템플릿 리터럴을 입력으로 받는 것
+// div태그에 적용하고 싶은 모든 스타일을 포함해야 하는 것
+const ControlContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1.5rem;
+` 
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,8 +35,8 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p>
+      <ControlContainer>
+        <p className="paragraph">
           <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
@@ -35,7 +48,7 @@ export default function AuthInputs() {
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
-        <p>
+        <p className="paragraph">
             <label className={`label ${emailNotValid ? 'invalid' : ''}`}>
                 Password
             </label>
@@ -47,7 +60,7 @@ export default function AuthInputs() {
                 }
             />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
