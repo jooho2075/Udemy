745a6ab30fc71a52fb4c905e4e313bf3b0968fb9
jooho2075 <parkjh2075@icloud.com>
2025-02-26 16:49:40 +0900

    25.02.26_재사용 가능한 Component 생성 및 Component 조합

--- React_Styling/styling/src/components/AuthInputs.jsx
+++ React_Styling/styling/src/components/AuthInputs.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 
+import Button from './Button.jsx';
+import Input from './Input.jsx';
+
 // div를 개별 component로 만들고 어떤 스타일이든 개발자가 적용하고 싶은
 // 스타일을 가지는 component로 만듬
 
@@ -11,42 +14,6 @@ const ControlContainer = styled.div`
     flex-direction: column;
     gap: 0.5rem;
     margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color:${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color:${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color:${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -71,29 +38,25 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <ControlContainer>
-        <p className="paragraph">
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            $invalid={emailNotValid}
-            type="email"
-            // 동적 및 조건적 Inline Style
-            // style={{
-            //     backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-            <Label $invalid={passwordNotValid}>Password</Label>
-            <Input
-              $invalid={passwordNotValid}
-              type="password"
-              onChange={(event) =>
-                handleInputChange('password', event.target.value)
-                }
-            />
-        </p>
+      <ControlContainer>  
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          // 동적 및 조건적 Inline Style
+          // style={{
+          //     backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+          // }}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+            }
+        />
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
