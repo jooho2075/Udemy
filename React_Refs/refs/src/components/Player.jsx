f3b1f549100bc1edb7f2e2bb544cae0c3fe926a5
jooho2075 <parkjh2075@icloud.com>
2025-02-27 17:42:14 +0900

    25.02.27_Ref소개 : HTML요소로 연결 및 접근

--- React_Refs/refs/src/components/Player.jsx
+++ React_Refs/refs/src/components/Player.jsx
@@ -1,10 +1,37 @@
+import { useState, useRef } from "react";
+
 function Player() {
+    const playerName = useRef();
+
+    const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+    // const [submitted, setSubmitted] = useState(false);
+    
+    /*
+    const handleChange = (event) => {
+        setSubmitted(false);
+        setEnteredPlayerName(event.target.value);
+    };
+    */
+   
+    const handleClick = () => {
+        setEnteredPlayerName(playerName.current.value); 
+        // 실제 참조값(ref)만 가짐
+        // current : input 요소를 값으로 보관하고 속성들에 접근할 수 있게됨
+    };
+
     return(
         <section id="player">
-            <h2>Welcome unknown entity</h2>
+            <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
             <p>
-                <input type="text" />
-                <button>Set Name</button>
+                <input 
+                    ref={playerName} 
+                    type="text"
+                    /* 
+                    onChange={handleChange} 
+                    value={enteredPlayerName}
+                    */
+                />
+                <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
     );
