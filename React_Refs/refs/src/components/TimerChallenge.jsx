3b6a72011e0347cdbf5c9ddbc38d88bb9484fcd7
jooho2075 <parkjh2075@icloud.com>
2025-03-08 12:33:15 +0900

    25.03.08_Timer설정 및 State관리

--- React_Refs/refs/src/components/TimerChallenge.jsx
+++ React_Refs/refs/src/components/TimerChallenge.jsx
@@ -1,21 +1,36 @@
+import { useState } from "react";
 
 // TimerChallenge component 목적 : 다른 설정들로 재사용될 수 있게 하는 것
-function TimerChallenge({title, targetTime}) { 
+function TimerChallenge({title, targetTime}) {
+    const [timerExpired, setTimerExpired] = useState(false);
+    const [timerStarted, setTimerStarted] = useState(false);
+
+    const handleStart = () => {
+        setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    };
+
+    const handleStop = () => {
+
+    };
+
     return(
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start challenge(도전 시작 또는 끝내기와 같은 형식으로 쓰여 있어야 함)
-                    나중에 useState로 관리할 부분
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className="active">
-                Time is running : 타이머가 실행 중일 경우 보여줌
-                Timer is inactive : 타이머가 실행 중이지 않을 경우 보여줌
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
             </p>
         </section>
     );
