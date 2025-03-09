cf1166a06612957fc3d6cc81ac5ac90843ac481d
jooho2075 <parkjh2075@icloud.com>
2025-03-09 17:27:45 +0900

    25.03.09_Result Modal 개선

--- React_Refs/refs/src/components/ResultModal.jsx
+++ React_Refs/refs/src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
 
-const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime}, ref, onReset) {
+const ResultModal = forwardRef(function ResultModal(
+    {targetTime, remainingTime}, 
+    ref, onReset
+) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
     const formattedRemainingtime = (remainingTime / 1000).toFixed(2); // toFixed() : 소수점 몇 자리까지만 표시되도록 하는 method
+    const score = Math.round((1 / remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return{
@@ -20,13 +24,14 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime},
         // <dialog> : 내장 스타일과 내장된 기능들 함께 제공
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score : {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} second</strong>
             </p>
             <p>
                 You stopped the timer with <strong>{formattedRemainingtime} seconds left</strong>
             </p>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
