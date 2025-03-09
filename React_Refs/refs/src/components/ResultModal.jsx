135aa758a7d97617db75eb1e96be6ef51347b6c3
jooho2075 <parkjh2075@icloud.com>
2025-03-09 16:02:51 +0900

    25.03.09_Modal Component 추가하기

---
+++ React_Refs/refs/src/components/ResultModal.jsx
@@ -0,0 +1,19 @@
+function ResultModal({result, targetTime}) {
+    return(
+        // <dialog> : 내장 스타일과 내장된 기능들 함께 제공
+        <dialog className="result-modal">
+            <h2>You {result}</h2>
+            <p>
+                The target time was <strong>{targetTime} second</strong>
+            </p>
+            <p>
+                You stopped the timer <strong>with X seconds left</strong>
+            </p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+}
+
+export default ResultModal;
