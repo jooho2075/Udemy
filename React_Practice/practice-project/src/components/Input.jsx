35d862b700fc26b722798666186e363099431264
jooho2075 <parkjh2075@icloud.com>
2025-03-14 21:52:31 +0900

    25.03.14_NewProject.jsx & Input.jsx 파일 생성

---
+++ React_Practice/practice-project/src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input({label, textarea, ...props}) {
+    return(
+        <p>
+            <label htmlFor="">{label}</label>
+            {textarea ? <textarea {...props}/>  : <input {...props}/>}
+        </p>
+    );
+}
+
+export default Input;
---
+++ React_Practice/practice-project/src/components/NewProject.jsx
@@ -0,0 +1,19 @@
+import Input from "./Input";
+
+function NewProject() {
+    return(
+        <div>
+            <menu>
+                <li><button>Cancel</button></li>
+                <li><button>Save</button></li>
+            </menu>
+            <div>
+                <Input label="Title"/>
+                <Input label="Description" textarea={true}/>
+                <Input label="Due Date"/>
+            </div>
+        </div>
+    );
+}
+
+export default NewProject;
