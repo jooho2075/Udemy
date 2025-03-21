db4f9e1e8894a16ece1ca73d3882497de74abbe6
jooho2075 <parkjh2075@icloud.com>
2025-03-21 14:27:47 +0900

    25.03.21_프로젝트 삭제 핸들링

--- React_Practice/practice-project/src/App.jsx
+++ React_Practice/practice-project/src/App.jsx
@@ -36,8 +36,8 @@ function App() {
       return {
         ...prevState,
         selectedProjectId: undefined,
-      }
-    })
+      };
+    });
   };
 
   const handleAddProject = (projectData) => {
@@ -56,9 +56,23 @@ function App() {
     });
   };
 
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject}/>;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
+  );
 
   if(projectsState.selectedProjectId === null) {
     content = (
