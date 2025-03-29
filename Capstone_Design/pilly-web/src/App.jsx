f1c137e63c38c10577b7d31d79250bd2b6f0fcea
jooho2075 <parkjh2075@icloud.com>
2025-03-30 02:34:39 +0900

    25.03.30_캡스톤 디자인 웹 시작

--- Capstone_Design/pilly-web/src/App.jsx
+++ Capstone_Design/pilly-web/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from './components/Header.jsx';
+
 function App() {
   return(
     <>
-      <h1>Hello World</h1>
+      <Header></Header>
     </>
   );
 }
---
+++ Capstone_Design/pilly-web/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <>
+            <h1>캡스톤 프로젝트 시작 예정</h1>
+        </>
+    );
+}
+
+export default Header;
