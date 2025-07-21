daba33f1dd202d508b0c97e89be2bff987a192b9
jooho2075 <parkjh2075@icloud.com>
2025-07-21 16:20:55 +0900

    25.07.21_174.Action 보내기 & useReducer로 State(상태) 수정하기(오류버전)

--- React_Context/src/store/shopping-cart-context.jsx
+++ React_Context/src/store/shopping-cart-context.jsx
@@ -11,6 +11,39 @@ export const CartContext = createContext({
 // CartContextProvider 함수 바깥에 만든 이유
 // => 앞서 만든 함수가 실행될 때마다 이 새로운 함수가 재생성되지 않도록 하기 위함
 function shoppingCartReducer(state, action) { // 인수가 2개인 이유 : action이 dispatch를 통해 보내진 후에 react가 reducer 함수를 호출할 것이기 때문
+    if(action.type === 'ADD_ITEM') {
+    }
+
+    if(action.type === 'UPDATE_ITEM') {
+         const updatedItems = [...state.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === action.payload.productId
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+        updatedItems.push({
+          id: action.payload,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        ...state, // not needed here because we have only one value
+        items: updatedItems,
+      };
+    }
+
     return state;
 };
 
@@ -28,58 +61,19 @@ export default function CartContextProvider({children}) {
   });
 
   function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
+    shoppingCartDispatch({
+        type: 'ADD_ITEM',
+        payload: id
     });
   }
 
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
+    shoppingCartDispatch({
+        type: 'UPDATE_ITEM',
+        pyaload: {
+            productId,
+            amount
+        }
     });
   }
 
